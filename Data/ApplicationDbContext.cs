e6a4e6b579bb54623e50640a787ad45b0cd7d2b8
nina <ninagbs@abv.bg>
2023-10-14 19:22:44 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Interview.Models;
+
+namespace Interview.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Interview.Models.Item>? Item { get; set; }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interview.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string ItemQuestion { get; set; }
+        public string ItemAnswer { get; set; }
+        public Item()
+        {
+
+        }
+    }
+}
